dfd6e784f36ca7a0dcb84d248ab733553d94afc6
Shrut <shrutpatel20@gmail.com>
2024-05-09 10:57:20 -0500

    added event listeners for all of the buttons

--- script.js
+++ script.js
@@ -26,12 +26,63 @@ function operate(operator, numOne, numTwo) {
   }
 }
 
+function addEventListeners() {
+  const buttons = document.querySelectorAll(".num");
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      if(!operatorPressed) {
+        addFirstDisplay(button.value);
+      }
+      else {
+        addSecondDisplay(button.value);
+      }
+    });
+  });
+}
+
+function addOperatorListeners () {
+  const operators = document.querySelectorAll(".op");
+  operators.forEach(op => {
+    op.addEventListener("click", () => {
+      operatorValue = op.value;
+      displayDiv.textContent = operatorValue;
+      operatorPressed = true;
+    });
+  })
+}
+
+function addFirstDisplay(value) {
+  if(displayValue === "0") {
+    displayValue = value;
+  } 
+  else {
+    displayValue += value;
+  }
+  displayDiv.textContent = displayValue;
+}
+
+function addSecondDisplay(value) {
+  secondDisplayValue += value;
+  displayDiv.textContent = secondDisplayValue;
+}
+
 let numOne;
 let operator;
 let numTwo;
+
+let displayValue = "0";
+let operatorValue = "";
+let secondDisplayValue = "";
+let operatorPressed = false;
+
+const displayDiv = document.querySelector(".display-container h1");
+displayDiv.textContent = displayValue;
 
 const btn = document.querySelector("#clear");
 
 btn.addEventListener("click", () => {
   alert("hey");
 });
+
+addEventListeners();
+addOperatorListeners();
